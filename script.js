6c005830b37bc152794d416b5cb21944149fd7a9
Sanjeev67p <sanjeevpatwal67@gmail.com>
2023-01-16 08:46:00 +0530

    commit

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-var skills = document.querySelector(".skills");
-var experience = document.querySelector(".experience");
-var education = document.querySelector(".education");
-var aa = document.querySelector(".a")
-var ba = document.getElementsByClassName(".b")
-var ca = document.querySelector(".c")
+var tabTitles = document.querySelectorAll(".tab-titles")
+var _skills = document.querySelector("._skills");
+var _experience = document.querySelector("._experience");
+var _education = document.querySelector("._education");
+var skills = document.querySelector(".skills")
+var experience = document.querySelector(".experience")
+var education = document.querySelector(".education")
 let yourChoice
 
-aa.addEventListener("click", fc);
-ca.addEventListener("click", cc);
-    function fc() {
-    
-    skills.classList.add("active-tab");
 
+//about me options
+skills.addEventListener("click", op1);
+experience.addEventListener("click", op2);
+education.addEventListener("click", op3);
+
+function op1() {
+    _education.classList.remove("active-tab");
+    _experience.classList.remove("active-tab");
+    _skills.classList.add("active-tab");
 };
-
-    function cc() {
-    
-    education.classList.add("active-tab");
-
+function op2() {
+    _education.classList.remove("active-tab");
+    _experience.classList.add("active-tab");
+    _skills.classList.remove("active-tab");
+};
+function op3() {
+    _skills.classList.remove("active-tab");
+    _experience.classList.remove("active-tab");
+    _education.classList.add("active-tab");
 };
