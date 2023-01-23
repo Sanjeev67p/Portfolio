9a0cd9071fc80bc2cfdb93dc57e1b9361f0a05df
Sanjeev67p <sanjeevpatwal67@gmail.com>
2023-01-23 10:50:10 +0530

    commit

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var tabTitles = document.querySelectorAll(".tab-titles")
-var _skills = document.querySelector("._skills");
-var _experience = document.querySelector("._experience");
-var _education = document.querySelector("._education");
-var skills = document.querySelector(".skills")
-var experience = document.querySelector(".experience")
-var education = document.querySelector(".education")
-let yourChoice
+// var tabTitles = document.querySelectorAll(".tab-titles")
+// var _skills = document.querySelector("._skills");
+// var _experience = document.querySelector("._experience");
+// var _education = document.querySelector("._education");
+// var skills = document.querySelector(".skills")
+// var experience = document.querySelector(".experience")
+// var education = document.querySelector(".education")
+let _home = document.querySelector("#_header")
+let _work = document.querySelector("#_work")
+let _contact = document.querySelector("#_contact")
 
+// rightnav
 
-//about options
-skills.addEventListener("click", op1);
-experience.addEventListener("click", op2);
-education.addEventListener("click", op3);
+_home.addEventListener("click", op_1);
+_work.addEventListener("click", op_2);
+_contact.addEventListener("click", op_3);
 
-
-function op1() {
-    _education.classList.remove("active-tab");
-    _experience.classList.remove("active-tab");
-    _skills.classList.add("active-tab");
+function op_1() {
+    _home.classList.add("opnav");
+    _work.classList.remove("opnav");
+    _contact.classList.remove("opnav");
 };
-function op2() {
-    _education.classList.remove("active-tab");
-    _experience.classList.add("active-tab");
-    _skills.classList.remove("active-tab");
+function op_2() {
+    _home.classList.remove("opnav");
+    _work.classList.add("opnav");
+    _contact.classList.remove("opnav");
 };
-function op3() {
-    _skills.classList.remove("active-tab");
-    _experience.classList.remove("active-tab");
-    _education.classList.add("active-tab");
+function op_3() {
+    _home.classList.remove("opnav");
+    _work.classList.remove("opnav");
+    _contact.classList.add("opnav");
 };
+
+// onclick=document.getElementById('_header').style.background ='red'
